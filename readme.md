d11ccefdb0da18f8dc090b84d4bc2d0b9b1b2d1d
chenglu <chenglu.she@gmail.com>
2023-04-17 14:52:55 +0800

    add simple readme

---
+++ readme.md
@@ -0,0 +1,21 @@
+# A Live Demo of Google - Isolated Sign Language Recognition
+
+Here is a live demo to try the Google Isolated Sign Language Recognition model inference with video you can try out.
+
+To use your own model, you should change the model path in the source code. (I'm think about how to make it more convenient to use your own model.)
+
+You can try a live demo with my model (LB 0.7) here: https://chenglu.me/gislr-live
+
+**Requirements**
+
+* nodejs 
+* vite
+
+**Setup and Run the demo**
+
+before you go, please change the model path in `/src/sign-language-recognition.jsx`
+
+```bash
+npm install
+npm run dev
+```
--- src/lib.jsx
+++ src/lib.jsx
@@ -1,10 +1,18 @@
 import SignLanguageRecognition from "./sign-language-recognition";
 import * as ReactDOM from "react-dom/client";
-import "./index.css"
+import styles from "./index.css?inline";
 
 class SignLanguageRecognitionElement extends HTMLElement {
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+  }
+
   connectedCallback() {
-    this.root = ReactDOM.createRoot(this);
+    this.root = ReactDOM.createRoot(this.shadow);
+    const styleSheet = new CSSStyleSheet();
+    styleSheet.replaceSync(styles);
+    this.shadow.adoptedStyleSheets = [styleSheet];
     this.root.render(<SignLanguageRecognition />);
   }
 
